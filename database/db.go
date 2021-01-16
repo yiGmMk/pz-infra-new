5676c1447cfe2af1ff25a370433f44dc2b06b980
yi_t <marvelousme@163.com>
2021-01-16 10:47:14 +0800

    日志记录说明

--- database/db.go
+++ database/db.go
@@ -16,51 +16,48 @@ import (
 	"gorm.io/gorm/utils"
 )
 
-var globalDB *gorm.DB
+var (
+	globalDB                 *gorm.DB
+	ErrRecordNotFound        = gorm.ErrRecordNotFound
+	ErrInvalidTransaction    = gorm.ErrInvalidTransaction
+	ErrNotImplemented        = gorm.ErrNotImplemented
+	ErrMissingWhereClause    = gorm.ErrMissingWhereClause
+	ErrUnsupportedRelation   = gorm.ErrUnsupportedRelation
+	ErrPrimaryKeyRequired    = gorm.ErrPrimaryKeyRequired
+	ErrModelValueRequired    = gorm.ErrModelValueRequired
+	ErrInvalidData           = gorm.ErrInvalidData
+	ErrUnsupportedDriver     = gorm.ErrUnsupportedDriver
+	ErrRegistered            = gorm.ErrRegistered
+	ErrInvalidField          = gorm.ErrInvalidField
+	ErrEmptySlice            = gorm.ErrEmptySlice
+	ErrDryRunModeUnsupported = gorm.ErrDryRunModeUnsupported
+)
 
-type MyLogger struct {
-	//logger logrus.Logger
-	logger        logging.Logger
-	SlowThreshold time.Duration
-	SourceField   string
+// gorm V2 日志输出需要实现logger.Interface接口
+type GormLogger struct {
+	logger        logging.Logger // logrus封装,日志记录
+	SlowThreshold time.Duration  // 慢查询阈值,用于慢查询日志记录
+	SourceField   string         //
 }
 
-func (l *MyLogger) LogMode(logger.LogLevel) logger.Interface {
+func (l *GormLogger) LogMode(logger.LogLevel) logger.Interface {
 	newlogger := *l
 	return &newlogger
 }
 
-func (l *MyLogger) Info(ctx context.Context, msg string, data ...interface{}) {
-	//l.logger.Info(msg, logging.With("data", append([]interface{}{}, data...)))
+func (l *GormLogger) Info(ctx context.Context, msg string, data ...interface{}) {
 	l.logger.WithContext(ctx).Infof(msg, data...)
 }
 
-func (l *MyLogger) Warn(ctx context.Context, msg string, data ...interface{}) {
-	//l.logger.Warn(msg, logging.With(msg, append([]interface{}{}, data...)))
+func (l *GormLogger) Warn(ctx context.Context, msg string, data ...interface{}) {
 	l.logger.WithContext(ctx).Warnf(msg, data...)
 }
 
-func (l *MyLogger) Error(ctx context.Context, msg string, data ...interface{}) {
-	//l.logger.Error(msg, logging.With("data", append([]interface{}{}, data...)))
+func (l *GormLogger) Error(ctx context.Context, msg string, data ...interface{}) {
 	l.logger.WithContext(ctx).Errorf(msg, data...)
 }
 
-func (l *MyLogger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
-	// elapsed := time.Since(begin)
-	// sql, rows := fc()
-	// if rows == -1 {
-	// 	l.logger.Info("trace info",
-	// 		logging.With("err", err),
-	// 		logging.With("time", float64(elapsed.Nanoseconds())/1e6),
-	// 		logging.With("sql", sql))
-	// } else {
-	// 	l.logger.Info("trace info",
-	// 		logging.With("err", err),
-	// 		logging.With("time", float64(elapsed.Nanoseconds())/1e6),
-	// 		logging.With("row", rows),
-	// 		logging.With("sql", sql))
-	// }
-
+func (l *GormLogger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
 	elapsed := time.Since(begin)
 	sql, _ := fc()
 	fields := logrus.Fields{}
@@ -81,26 +78,10 @@ func (l *MyLogger) Trace(ctx context.Context, begin time.Time, fc func() (string
 	l.logger.WithContext(ctx).WithFields(fields).Debugf("%s [%s]", sql, elapsed)
 }
 
-var (
-	ErrRecordNotFound        = gorm.ErrRecordNotFound
-	ErrInvalidTransaction    = gorm.ErrInvalidTransaction
-	ErrNotImplemented        = gorm.ErrNotImplemented
-	ErrMissingWhereClause    = gorm.ErrMissingWhereClause
-	ErrUnsupportedRelation   = gorm.ErrUnsupportedRelation
-	ErrPrimaryKeyRequired    = gorm.ErrPrimaryKeyRequired
-	ErrModelValueRequired    = gorm.ErrModelValueRequired
-	ErrInvalidData           = gorm.ErrInvalidData
-	ErrUnsupportedDriver     = gorm.ErrUnsupportedDriver
-	ErrRegistered            = gorm.ErrRegistered
-	ErrInvalidField          = gorm.ErrInvalidField
-	ErrEmptySlice            = gorm.ErrEmptySlice
-	ErrDryRunModeUnsupported = gorm.ErrDryRunModeUnsupported
-)
-
 func InitDB(connectString string, infraLogger logging.Logger) error {
 	var gormlog logger.Interface
 	if infraLogger != nil {
-		gormlog = &MyLogger{
+		gormlog = &GormLogger{
 			SlowThreshold: 200 * time.Millisecond,
 			logger:        infraLogger,
 		}
--- logging/logger.go
+++ logging/logger.go
@@ -11,6 +11,7 @@ type Logger interface {
 	Debug(message string, fields ...Field)
 	Info(message string, fields ...Field)
 	Warn(message string, fields ...Field)
+	// 无堆栈信息错误使用,已携带堆栈信息的error不应使用该函数,会输出双份的堆栈信息
 	Error(message string, fields ...Field) error
 	// Fatal logs a message, then calls os.Exit(1).
 	Fatal(message string, fields ...Field)
@@ -21,7 +22,10 @@ type Logger interface {
 
 	WithContext(ctx context.Context) *logrus.Entry
 
+	// github.com/pkg/errors
+	// errors.New/WithStack/Wrap/Wrapf等可以方便地给错误带上堆栈信息,方便定位错误
 	// 已携带堆栈信息的错误,使用fmt.Sprintf("%+v",err)得到完整的错误信息输出
+	// 未包含堆栈信息的错误请使用 Error()输出日志,会带上堆栈信息
 	LogErrorHasStackInfo(err error, args ...interface{})
 }
 
